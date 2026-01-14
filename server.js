b6e158ac8e1d3b9bbcb6b34a5637ce6d69f53850
glentechkenya <glentech287@gmail.com>
2026-01-14 17:23:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ const io = require("socket.io")(http);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
+  let username = "Anonymous";
+
+  socket.on("join", (name) => {
+    username = name;
+    socket.broadcast.emit("message", `🟢 ${username} joined Codex Heist`);
+  });
+
   socket.on("message", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", `${username}: ${msg}`);
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("message", `🔴 ${username} left Codex Heist`);
   });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log("Codex Heist server running");
+  console.log("Codex Heist running");
 });
